8e159fde7433d982bae2d8b9682a649fcddf358b
Matthew Tran <matt.h.tran@gmail.com>
2024-01-06 01:44:10 -0500

    to.dict changes

--- server/models.py
+++ server/models.py
@@ -19,14 +19,6 @@ class User(db.Model, SerializerMixin):
     reviews_received = relationship('Review', back_populates='reviewed_user', foreign_keys='Review.reviewed_user_id', lazy=True)
     reviews_given = relationship('Review', back_populates='reviewing_user', foreign_keys='Review.reviewing_user_id', lazy=True)
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'username': self.username,
-            'email': self.email,
-            'password': self.password,
-        }
 
 class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
@@ -41,14 +33,7 @@ class Item(db.Model, SerializerMixin):
     owner = relationship('User', back_populates='items')
     trades = relationship('Trade', secondary='trade_item_association', back_populates='item')
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-            'description': self.description,
-            'owner_id': self.owner_id,
-            'type_id': self.type_id,
-        }
+
 
 class Trade(db.Model, SerializerMixin):
     __tablename__ = 'trades'
@@ -75,15 +60,6 @@ class Trade(db.Model, SerializerMixin):
     receiver = relationship('User', back_populates='trades_received', foreign_keys=[receiver_id])
     item = relationship('Item', secondary='trade_item_association', back_populates='trades')
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'initiator_id': self.initiator_id,
-            'receiver_id': self.receiver_id,
-            'status': self.status,
-            'created_at': self.created_at.isoformat(),
-            'trade_cost': self.trade_cost,
-        }
 
 class Review(db.Model, SerializerMixin):
     __tablename__ = 'reviews'
@@ -102,15 +78,7 @@ class Review(db.Model, SerializerMixin):
     reviewing_user = relationship('User', back_populates='reviews_given', foreign_keys=[reviewing_user_id])
     reviewed_user = relationship('User', back_populates='reviews_received', foreign_keys=[reviewed_user_id])
 
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'reviewing_user_id': self.reviewing_user_id,
-            'reviewed_user_id': self.reviewed_user_id,
-            'text': self.text,
-            'rating': self.rating,
-            'created_at': self.created_at.isoformat(),
-        }
+   
 
 class ItemType(db.Model, SerializerMixin):
     __tablename__ = 'item_types'
@@ -118,9 +86,3 @@ class ItemType(db.Model, SerializerMixin):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True, nullable=False)
     items = relationship('Item', back_populates='item_type')
-
-    def to_dict(self):
-        return {
-            'id': self.id,
-            'name': self.name,
-        }
